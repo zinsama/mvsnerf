ec21b8b2e856efed8742b15f633e98af8ce5b8c8
zkf20 <zkf20@mails.tsinghua.edu.cn>
2022-08-11 04:56:30 +0800

    fft高通低通过滤

--- video_data/pic/fft_mask.py
+++ video_data/pic/fft_mask.py
@@ -8,7 +8,9 @@ os.makedirs(output_path,exist_ok=True)
 
 img = cv2.imread(file_path)
 h,w = img.shape[:2]
-print(img.shape)
+
+
+
 #生成低通和高通滤波器
 lpf = np.zeros((h,w,3))
 R = (h+w)//8  #或其他
@@ -25,12 +27,12 @@ hf = freq * hpf
 
 #生成低频分量图
 img_l = np.abs(np.fft.ifft2(lf,axes=(0,1)))
-img_l = np.clip(img_l,0,255) #会产生一些过大值需要截断
+# img_l = np.clip(img_l,0,255) #会产生一些过大值需要截断
 img_l = img_l.astype('uint8')
 cv2.imwrite(output_path+'/LPF.jpg',img_l)
 #生成高频分量图
 img_h = np.abs(np.fft.ifft2(hf,axes=(0,1)))
-img_h = np.clip(img_h,0,255) #似乎一般不会超，加上保险一些
+# img_h = np.clip(img_h,0,255) #似乎一般不会超，加上保险一些
 img_h = img_h.astype('uint8')
 cv2.imwrite(output_path+'/HPF.jpg',img_h)
 #画出频谱图
